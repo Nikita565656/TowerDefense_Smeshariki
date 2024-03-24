aec6578dc9354efc60d50c1e7a8a2899f5a0cac9
Nikita565656 <164736311+Nikita565656@users.noreply.github.com>
2024-03-24 11:46:33 +0100

    Coomit

--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -7,10 +7,10 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
