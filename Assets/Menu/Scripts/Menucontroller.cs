0b3d852f31db32de26b9af7e209f3b8409f4715f
Nikita565656 <164736311+Nikita565656@users.noreply.github.com>
2024-03-24 12:48:06 +0100

    Menu + Levela

---
+++ Assets/Menu/Scripts/Menucontroller.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menucontroller : MonoBehaviour 
+{
+    
+    
+    public void LoadSceneMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void LoadSceneGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void LoadSceneLevels()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+    public void Quit()
+    {
+       Application.Quit();
+    }
+}
