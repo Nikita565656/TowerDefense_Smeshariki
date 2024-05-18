d6b8f917122c1bbedcd7d4605f3b07e66a0d1f29
Nosochek12365 <164626939+Nosochek12365@users.noreply.github.com>
2024-05-18 12:45:24 +0300

    commit

--- Assets/_Scripts/LevelScripts/GameManager.cs
+++ Assets/_Scripts/LevelScripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class SceneSwitcher : MonoBehaviour
 {
     public Button yourButton; // кнопка на канвасе
+    public Text yourText; // текст на канвасе
     public int enemyKillCount = 5; // количество врагов, которых нужно убить для переключения сцены
     private int currentKillCount = 0;
 
@@ -13,31 +14,42 @@ public class SceneSwitcher : MonoBehaviour
         yourButton.onClick.AddListener(SwitchScene);
         Enemy.OnEnemyKilled += EnemyKilled;
 
-        // Сделайте кнопку невидимой в начале
+        // Сделайте кнопку и текст невидимыми в начале
         yourButton.gameObject.SetActive(false);
+        yourText.gameObject.SetActive(false);
     }
 
     private void EnemyKilled()
     {
         currentKillCount++;
 
-        // Если достигнуто необходимое количество убийств, сделайте кнопку видимой
+        // Если достигнуто необходимое количество убийств, сделайте кнопку и текст видимыми
         if (currentKillCount >= enemyKillCount)
         {
+            Time.timeScale = 0;
             yourButton.gameObject.SetActive(true);
+            yourText.gameObject.SetActive(true);
+            
         }
     }
 
     private void SwitchScene()
     {
-        // Если текущая сцена - "2 level", переключите на "3 level". В противном случае переключите на "2 level".
-        if (SceneManager.GetActiveScene().name == "2 level")
+        Time.timeScale = 1;
+        if (SceneManager.GetActiveScene().buildIndex == 0)
         {
-            SceneManager.LoadScene("3 level");
+            SceneManager.LoadScene(1);
         }
-        else
+        else if(SceneManager.GetActiveScene().buildIndex == 1)
         {
-            SceneManager.LoadScene("2 level");
+
+            SceneManager.LoadScene(2);
+           
+        }
+        else if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+
+        SceneManager.LoadScene(0);
         }
     }
 
